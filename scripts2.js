5ea09f8e2edb7d8d766cfc765f84e3d12c53b12b
Start Coding Student 2 <session2@START-Coding-MBP-13.local>
2015-10-23 17:00:34 -0700

    Finished code

---
+++ scripts2.js
@@ -0,0 +1,46 @@
+function changeDescription () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p1').innerHTML = newDescription;
+
+}
+function changeDescription2 () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p2').innerHTML = newDescription;
+
+}
+function changeDescription3 () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p3').innerHTML = newDescription;
+
+}
+function changeDescription4 () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p4').innerHTML = newDescription;
+
+
+}
+function changeDescription5 () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p5').innerHTML = newDescription;
+
+}
+function changeDescription6 () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p6').innerHTML = newDescription;
+
+}
+function changeDescription7 () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p7').innerHTML = newDescription;
+
+}
+function changeDescription8 () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p8').innerHTML = newDescription;
+
+}
+function changeDescription9 () {
+    var newDescription = prompt("What is this going to say?");
+    document.getElementById('p9').innerHTML = newDescription;
+
+}
